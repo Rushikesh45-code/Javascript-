97dddac6fc0c8a890c74cbba4d2a8ad7fb39ec27
Rushikesh45-code <rushikeshhs45gmail.com>
2025-08-16 00:02:42 +0530

    Refactor Test.js to initialize variables and add logging for state changes

--- Test.js
+++ Test.js
@@ -1,1 +1,9 @@
-console.log("Big Pow")
+let bala
+var ArtificalElectronics = "AI" // not good 
+const BigPow = 1;
+console.log("Before");
+console.table(["HI", "Hello", BigPow, bala, ArtificalElectronics]);
+
+bala = 10;
+console.log("After");
+console.table(["HI", "Hello", BigPow, bala, ArtificalElectronics]);
