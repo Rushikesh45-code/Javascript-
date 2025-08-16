7c52cf3caea9a3d7ec78a7f78970300b778b97a4
Rushikesh45-code <rushikeshhs45gmail.com>
2025-08-17 00:20:09 +0530

    Datatype

---
+++ 2_DataTypes.js
@@ -0,0 +1,19 @@
+"use strict";// By this we tell we are using newer version code 
+             // But mist of the By default code is in newer version
+
+
+// Data Types
+
+let name = "Bala"; // string -> "" or ''
+let age = 17; // number -> Has the capisity of 2^53
+let alive = true;// boolean -> true / false
+let job = null;// null -> Standalone value. noting, not 0 / +ve / -ve
+let salary;// undefined -> not define any datatype.
+// symbol -> unique . This is used in figma like apps (react)
+   
+
+// For finding datatype we can use and output will be it's datatype
+console.log(typeof name);
+console.log(typeof job);// object
+console.log(typeof salary);
+
