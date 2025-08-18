e202c63f13b5668f9eba439dee72eb5ffe5bf962
Rushikesh45-code <rushikeshhs45gmail.com>
2025-08-18 22:17:10 +0530

    conversions of Data types

---
+++ Basics/3_conversion.js
@@ -0,0 +1,23 @@
+let score = "45rs"
+console.log(typeof(score));// string
+console.log(score);
+
+let stringscore = Number(score)
+console.log(typeof(stringscore));// number
+console.log(stringscore);// NaN
+
+let login = 45
+console.log(typeof(login),login);// number -1000
+
+let numberlogin = Boolean(login)
+console.log(typeof(numberlogin),numberlogin);// if 0 -> false and non zero num -> true
+
+let name = " "
+console.log(typeof(name), name);// string bala
+
+let boolstirng = Boolean(name)
+console.log(typeof(boolstirng), boolstirng);// boolean 1 // if "bala"(non empty string) -> 1 else -> 0
+
+//--------------------------------------------Operation--------------------------------------------------------
+
+
