bfe6ca7c3158430c036006164dd822a4b0a34271
Rushikesh45-code <rushikeshhs45gmail.com>
2025-08-19 00:09:18 +0530

    compair

---
+++ Basics/4_Compairson.js
@@ -0,0 +1,11 @@
+//console.log(true != false);
+
+
+// let num ;
+// let numnum = Number(num)
+// console.log(typeof numnum);
+
+console.log("2" === 2); // === -> stricty checks the both data types and value 
+
+// compairson between different data types is bad practice
+// it will become so complicated therefore do compairson between same data types
